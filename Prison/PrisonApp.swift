82e64092d4a4f1598ba42310258d5680fbeb0dc3
haudevios <nguyenduchaudeptrai2003@gmail.com>
2025-06-18 15:41:52 +0700

    Initial Commit

---
+++ Prison/PrisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrisonApp.swift
+//  Prison
+//
+//  Created by umaxsoft on 18/6/25.
+//
+
+import SwiftUI
+
+@main
+struct PrisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrisonTests/PrisonTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrisonTests.swift
+//  PrisonTests
+//
+//  Created by umaxsoft on 18/6/25.
+//
+
+import Testing
+@testable import Prison
+
+struct PrisonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
